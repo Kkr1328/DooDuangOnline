9924ec47b1788ab857692d27c3645eed0b68cd63
Chinnapat.N <kharkkr@gmail.com>
2023-01-23 14:54:49 +0700

    Add form page

---
+++ src/routes/Form.svelte
@@ -0,0 +1,56 @@
+<script>
+    import Textfield from '@smui/textfield';
+    import Icon from '@smui/textfield/icon';
+    import HelperText from '@smui/textfield/helper-text';
+  
+    let name = '';
+    let surname = '';
+    let birthdate = '';
+</script>
+
+<style>
+  h1 {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  .user_form {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+</style>
+
+<h1>
+  Fill information
+</h1>
+<div class="user_form">
+  <div>
+    <Textfield bind:value={name} label="Name">
+      <HelperText slot="helper">Helper Text</HelperText>
+    </Textfield>
+ 
+    <pre class="status">Value: {name}</pre>
+  </div>
+  <div>
+    <Textfield bind:value={surname} label="Surname">
+      <HelperText slot="helper">Helper Text</HelperText>
+    </Textfield>
+ 
+    <pre class="status">Value: {surname}</pre>
+  </div>
+  <div>
+    <Textfield bind:value={birthdate} label="Birth Date">
+      <HelperText slot="helper">Helper Text</HelperText>
+    </Textfield>
+ 
+    <pre class="status">Value: {birthdate}</pre>
+  </div>
+</div>
